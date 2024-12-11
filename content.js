79e207ad189b5087d093a7e7b874f5759f027e6d
Nick Castro <ncastro878@gmail.com>
2024-12-10 20:49:33 -0600

    another new laptop test

--- content.js
+++ content.js
@@ -6,6 +6,7 @@ function getSelectedTextFromPDF() {
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
+
   if (request.action === "getSelectedText") {
     const selectedText = getSelectedTextFromPDF();
     sendResponse({selectedText: selectedText});
